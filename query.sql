f93e4f67e04c351ce5123c784844d33a2f14017e
tariqul420 <tariqul.developer@gmail.com>
2025-06-12 22:16:25 +0600

    Refactor SQL queries to include additional examples for aliases, distinct values, and conditional selections; correct order by syntax and offset value

--- query.sql
+++ query.sql
@@ -7,3 +7,25 @@ SELECT id, name, email FROM users;
 -- select everything a user whose id is 4
 SELECT * FROM users WHERE id = 4;
 
+SELECT name, email FROM users WHERE id = 4;
+
+-- ===============================
+-- aliases, distict, and, or, limit, offset, order by
+
+-- aliases (rename columns name)
+SELECT name as user_name FROM users;
+
+-- distict (only give unique value)
+SELECT DISTINCT name FROM users;
+
+-- and 
+SELECT * FROM users WHERE age >= 25 AND district = 'dhaka';
+
+-- or
+SELECT * FROM users WHERE age >=25 OR district = 'dhaka';
+
+-- limit and offset(skip)
+SELECT * FROM users LIMIT 1 OFFSET 1
+
+-- order by
+SELECT * FROM users BY age DESC LIMIT 3 OFFSET 6
