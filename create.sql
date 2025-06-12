9f19aac89e98818192a5c1bd5c0850d8012ff4c5
tariqul420 <tariqul.developer@gmail.com>
2025-06-12 21:32:27 +0600

    Update SQL script to correct active database reference and enhance users table structure

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
--- Active: 1749729094656@@127.0.0.1@5432@postgres
+-- Active: 1749742022445@@127.0.0.1@5432@first_db
 
 -- 1. database creation
 -- 2. table creation
@@ -12,13 +12,33 @@ CREATE DATABASE first_db;
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(30),
-  age INT,
   email VARCHAR(80) UNIQUE,
-  address TEXT
+  contactNo VARCHAR(11),
+  password VARCHAR(12),
+  createdAt DATE DEFAULT NOW()
 );
 
--- see all from users table
+-- drop database
+DROP DATABASE first_db;
+
+-- drop table
+DROP TABLE users;
+
+-- see all user from users table
 SELECT * FROM users;
 
--- drop data
-DROP DATABASE first_db;
+-- ===============================================
+
+-- current database
+-- show all database
+-- insert data into user table
+
+-- current database
+SELECT current_database();
+
+-- show all database
+SELECT datname FROM pg_database;
+
+-- insert into table_name (columns) values ()
+INSERT INTO users(name, email, contactNo, password) VALUES('Tariqul1', 'tariqul@gmail1.com', '01743892058', '123456'),
+('Tariqul2', 'tariqul2@gmail.com', '01743892058', '123456');
